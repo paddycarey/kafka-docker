a539f49b535b94fb37f9fd4567b38a1f68e7930b
Dean Barnett <dean@shopkeep.com>
2017-02-24 13:11:51 +0000

    Expose SSL Port

--- scripts/start_kafka.sh
+++ scripts/start_kafka.sh
@@ -3,9 +3,10 @@ set -e
 
 # Set the advertised host value (if set)
 if [ ! -z "$KAFKA_ADVERTISED_HOST" ]; then
-    echo "Setting advertised.listeners value to: PLAINTEXT://$KAFKA_ADVERTISED_HOST:9092 or SASL_SSL://$KAFKA_ADVERTISED_HOST:9193"
+    echo "Setting advertised.listeners value to: PLAINTEXT://$KAFKA_ADVERTISED_HOST:9092 or SSL://$KAFKA_ADVERTISED_HOST:9093 or SASL_SSL://$KAFKA_ADVERTISED_HOST:9193"
     sed -i.bak "s/advertised.listeners=PLAINTEXT:\/\/localhost:9092/advertised.listeners=PLAINTEXT:\/\/$KAFKA_ADVERTISED_HOST:9092/g" /etc/kafka/server.properties
     sed -i.bak "s/SASL_SSL:\/\/localhost:9193/SASL_SSL:\/\/$KAFKA_ADVERTISED_HOST:9193/g" /etc/kafka/server.properties
+    sed -i.bak "s/SSL:\/\/localhost:9093/SSL:\/\/$KAFKA_ADVERTISED_HOST:9093/g" /etc/kafka/server.properties
     echo "127.0.0.1 $KAFKA_ADVERTISED_HOST" >> /etc/hosts
 fi
 
